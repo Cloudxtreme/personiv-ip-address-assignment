922ec15e528dcec63f9600aeb12bea4adca9cc40
Marco Paco <marcopaco1230@gmail.com>
2016-01-14 19:54:55 +0800

    2nd bug fix

--- database/migrations/2016_01_14_101509_create_network_switches_table.php
+++ database/migrations/2016_01_14_101509_create_network_switches_table.php
@@ -15,7 +15,7 @@ class CreateNetworkSwitchesTable extends Migration
         Schema::create('network_switches', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
